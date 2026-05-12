a30edded645962f03752c27c0c3c217461a1f4a7
Ahmad Sultan <sultan.ahmadsiall@gmail.com>
2026-05-13 02:26:46 +0500

    Add discount calculation feature

---
+++ discount.py
@@ -0,0 +1,8 @@
+﻿def get_discount(price, user_type):
+    if user_type == "admin":
+        discount = 0.5
+    elif user_type == "member":
+        discount = 0.1
+    return price * discount
+
+print(get_discount(100, "guest"))
